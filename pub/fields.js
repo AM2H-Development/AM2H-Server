3f2b1bffe0bbbf09acf5da5742eb8dc67c6a8fc7
martinhetschel <martin.hetschel@mach1kart.de>
2017-04-14 01:32:42 +0200

    new fields

--- pub/fields.js
+++ pub/fields.js
@@ -2,19 +2,27 @@
 Felddefinition
 V1.0.0 vom 10.04.2017
 */
-/* global Function, NaN */
+/* global Function, ds */
+
+var socket = io();
 
 $(document).ready(function () {
-    initAll();
+    initFields();
 });
 
+const _l = new Map();
+const _v = new Map();
+
 class DF {
-    constructor(args,style,renderer,compute){
+    constructor(args,style,renderer,compute,formatter,prescale=1,fraction=0,unit=""){
         this.id=null;
         this.status=0; // 0=not initialized, 1=initialized and valid, 2=initialized need redraw
         this.style=style;
         this.args=args;
         this.value=null;
+        this.prescale=prescale;
+        this.fraction=fraction;
+        this.unit=unit;
         if (renderer instanceof Function){
             this.renderer=renderer;
         } else {
@@ -23,7 +31,20 @@ class DF {
         if (compute instanceof Function){
             this.compute=compute;
         } else {
-            this.compute=function(){return arguments[0];};
+            this.compute=function(args){return _v.get(args[0]);};
+        }
+        console.log("formatter: " + formatter);
+        if (formatter instanceof Function){
+            this.formatter=formatter;
+        } else {
+            this.formatter=function(val,prescale,fraction,unit){
+                val =  (val+" ").replace(",", ".");
+                val /= prescale;
+                return val.toLocaleString(
+                        'de-DE', 
+                        {minimumFractionDigits: fraction, maximumFractionDigits: fraction}
+                        ) + unit;
+            };
         }
     }
     
@@ -34,7 +55,7 @@ class DF {
                 this.status=1;
                 break;
             case 2:
-                $(context).html(this.renderer(this.id,this.value,this.style));
+                $("#"+this.id).html(this.renderer(this.id,this.formatter(this.value,this.prescale,this.fraction,this.unit),this.style));
                 this.status=1;
                 break;
         }
@@ -42,59 +63,71 @@ class DF {
     }    
 }
 
-var df = function(id,val,style){return "<div id=\""+id+"\" style=\""+style+"\">Wert: "+val+"</div>";};
-var cp = function(){return arguments[0]*2;};
-
-class Value{
+class Listener{
     constructor(id){
-        this.value= new Set();
-        this.value.add(id);
+        this.listener= new Set();
+        this.listener.add(id);
+    }
+    addListener(id){
+        this.listener.add(id);
     }
 }
 
-class Values{
-    constructor(){
-        this.values = new Map();
-    }    
-    addTopic(topic,id){
-        if ( this.values.get(topic) ){
-            this.values.get(topic).value.add(id);
-        } else {
-            this.values.set(topic, new Value(id));
-        }
-    }
-}
 class Container {
-    constructor(context){
+    constructor(){
         this.id=0;
-        this.context = context;
+        this.defVal="wait...";
+        this.context = null;
         this.container= new Set();
-        this.values= new Values();
     }
-    addDF(defVal,args,style,renderer,compute){
-        var df = new DF(args,style,renderer,compute);
+    setContext(context){
+        this.context=context;
+    }
+    setDefaultValue(defVal){
+        this.defVal=defVal;
+    }
+    addDF(args,style,renderer,compute,formatter,prescale,fraction,unit){
+        var df = new DF(args,style,renderer,compute,formatter,prescale,fraction,unit);
         df.id="df"+this.id++;
-        df.value=defVal;
+        df.value=this.defVal;
         this.container.add(df);
-        this.values.addTopic(df.args,df.id);
-    }
-    update(){
-        for (let item of this.container) {
-            console.log(item.id);
-            item.update(this.context);
+        for (var i = 0, len = args.length; i < len; i++) {
+            this.addTopic(args[i],df);
         }
     }
-    
+    addTopic(topic,df){
+        _v.set(topic,null);
+        if ( _l.get(topic) ){
+            _l.get(topic).addListener(df);
+        } else {
+            _l.set(topic, new Listener(df));
+            socket.on(topic,this.updateValue);
+            console.log("poll "+topic);
+            socket.emit('poll',topic);
+        }
+    }
+    updateValue(topicvalue){
+        var split = topicvalue.split('#', 2);
+        var topic = split[0];
+        var value = split[1];
+        console.log("Listener "+topic+" : "+value);
+        _v.set(topic,value);
+        console.log(_l.get(topic).listener);
+        for (let df of _l.get(topic).listener){
+            console.log(df.id);
+            console.log(df.compute(df.args));
+            df.value = df.compute(df.args);
+            df.status=2;
+        }
+        c.init();
+    }
+    init(){
+        for (let df of this.container) {
+            console.log(df.id);
+            df.update(this.context);
+        }
+    }
 }
+const c = new Container();
 
 
-function initAll(){
-    var defVal="wait..";
-    var c = new Container("#contentlayer");
-
-    c.addDF(defVal,"/mh/location","display:inline;" );
-    c.addDF(defVal,"/mh/location","",df );
-    c.addDF(defVal,"/mh/location","",df,cp );
-
-    c.update();
-}
