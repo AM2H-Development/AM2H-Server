781cb4c58e09425122a3d3706127c915366d4250
jq7140 <am@clicca.de>
2017-04-14 18:56:44 +0200

    New Channels

--- pub/dfakm.js
+++ pub/dfakm.js
@@ -4,7 +4,7 @@
 /* global c, v */
 
 var bgImage = {
-    "background-image": "url(\"http://qxf.de/HeizungV2_image.svg\")",
+    "background-image": "url(\"http://clicca.de/StromV2_image.svg\")",
     "width": "1030px",
     "height": "620px",
     "background-size": "1030px 620px"
@@ -18,9 +18,25 @@ var re = function(id,val,style){
     }
     return "<div class=\"df\" id=\""+id+"\" style=\""+style+"\">"+val+"</div>";
 };
+var re1 = function(id,val,style){
+    if (parseInt(val)<0){
+        style +="background-color: lightgreen;"
+    } else {
+        style +="background-color: lightyellow;"
+           }
+    return "<div class=\"df\" id=\""+id+"\" style=\""+style+"\">"+val+"</div>";
+};
+var re2 = function(id,val,style){
+    if (parseInt(val)<0){
+        style +="background-color: lightgreen;"
+    } else {
+        style +="background-color: lightyellow;"
+           }
+    return "<div class=\"df\" id=\""+id+"\" style=\""+style+"\">"+val+"</div>";
+};
 var cp = function(a){
     var val = v.asF(a[0])-v.asF(a[1]);
-    if (val + v.asF(a[1])>0) val *=-1; 
+    // if (val + v.asF(a[1])>0) val *=-1; 
     return val;
 };
 var fo = function(val,prescale,fraction,unit){
@@ -34,11 +50,15 @@ function initFields(){
     c.setBgImage(bgImage);
     c.setDefaultValue("wait..");
     c.addDF(["akm/d04/state/temp1"],"width: 56px; left:  10px; top: 200px;"," °C" );
-    c.addDF(["akm/d04/state/hum1"],"width: 56px; left:  10px; top: 225px;"," °C" );
+    c.addDF(["akm/d04/state/hum1"],"width: 56px; left:  10px; top: 225px;"," %" );
     c.addDF(["akm/d04/state/temp2"],"width: 56px; left:  10px; top: 250px;"," °C" );
-    c.addDF(["akm/d04/state/hum2"],"width: 56px; left:  10px; top: 275px;"," °C" );
-    c.addDF(["akm/m02/state/power01"],"width: 86px; left:  10px; top: 120px;"," W",null,null,null,100,1);
-    c.addDF(["akm/m03/state/power01"],"width: 86px; left:  10px; top: 150px;"," W",null,null,null,100,1);
-    c.addDF(["akm/m02/state/power01","akm/m03/state/power01"],"width: 86px; left:  10px; top: 175px;"," W",re,cp,null,100,1); 
+    c.addDF(["akm/d04/state/hum2"],"width: 56px; left:  10px; top: 275px;"," %" );
+    c.addDF(["akm/m01/state/power01"],"width: 86px; left:  10px; top: 350px;"," kW",null,null,null,100,1 );
+    c.addDF(["akm/m01/state/counter01"],"width: 86px; left:  10px; top: 375px;"," m³/d",null,null,null,100,2  );
+    c.addDF(["akm/m01/state/counter02"],"width: 86px; left:  10px; top: 400px;"," m³/m",null,null,null,100,2 );
+    c.addDF(["akm/m01/state/counter03"],"width: 86px; left:  10px; top: 425px;"," m³",null,null,null,100,2 );
+    c.addDF(["akm/m02/state/power01"],"width: 78px; left:  212px; top: 363px;"," W",re1,null,null,100,1);
+    c.addDF(["akm/m03/state/power01"],"width: 78px; left:  425px; top: 363px;"," W",re2,null,null,100,1);
+    c.addDF(["akm/m02/state/power01","akm/m03/state/power01"],"width: 78px; left:  212px; top: 307px;"," W",null,cp,null,100,1); 
      
 }    
