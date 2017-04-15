80cc32664ae00505664ceb1a3b8aec6f03fbe6a1
martinhetschel <martin.hetschel@mach1kart.de>
2017-04-15 16:24:27 +0200

    config.sample erzeugt

---
+++ cfg/config.sample.js
@@ -0,0 +1,12 @@
+/* 
+ * Config
+ */
+
+var config = {
+    host          : "192.168.178.XXX",
+    mysqlUser     : 'pi',
+    mysqlPassword : 'XXX',
+    database      : 'XXX'
+};
+module.exports = config;
+
